99b910ead1c7d304e55ca8e8110de16ee6acac50
Rohan Julka <rohanjulka19@gmail.com>
2024-03-12 00:50:25 +0000

    Differentiate for loop conditions in reverse mode (vgvassilev#746)
    This change differentiates the loop condition expression.
    Additionaly if in forward pass a loop terminates pre-maturely due to break stmt.
    The reverse pass should start differentiation with break statment and
    not the loop condition differentiation. This change keeps track of whether the break
    was called in forward pass and based on that in reverse mode it is decided
    whether the loop differentiation is skipped for the first iteration or not.

--- test/Gradient/Gradients.C
+++ test/Gradient/Gradients.C
@@ -653,13 +653,21 @@ float running_sum(float* p, int n) {
 // CHECK-NEXT:     int i = 0;
 // CHECK-NEXT:     clad::tape<float> _t1 = {};
 // CHECK-NEXT:     _t0 = {{0U|0UL}};
-// CHECK-NEXT:     for (i = 1; i < n; i++) {
+// CHECK-NEXT:     for (i = 1; ; i++) {
+// CHECK-NEXT:         {
+// CHECK-NEXT:             if (!(i < n))
+// CHECK-NEXT:                 break;
+// CHECK-NEXT:         }
 // CHECK-NEXT:         _t0++;
 // CHECK-NEXT:         clad::push(_t1, p[i]);
 // CHECK-NEXT:         p[i] += p[i - 1];
 // CHECK-NEXT:     }
 // CHECK-NEXT:     _d_p[n - 1] += 1;
-// CHECK-NEXT:     for (; _t0; _t0--) {
+// CHECK-NEXT:     for (;; _t0--) {
+// CHECK-NEXT:         {
+// CHECK-NEXT:             if (!_t0)
+// CHECK-NEXT:                 break;
+// CHECK-NEXT:         }
 // CHECK-NEXT:         i--;
 // CHECK-NEXT:         {
 // CHECK-NEXT:             p[i] = clad::pop(_t1);
